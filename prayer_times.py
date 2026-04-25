ff96601271e64c181a1cad59dfc773bf75428baa
aboodahdab <abodkaramma77@gmail.com>
2026-04-25 14:03:04 +0300

    implemented caching

--- prayer_times.py
+++ prayer_times.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env /Users/abdullatif/swiftbar_folder/venv/bin/python
+# echo "☀️ My Plugin"
+# echo "---"
+
+# This item has a submenu — hovering/clicking it reveals options to the side
+# echo "Appearance"
+# echo "-- Disable Dark Mode | bash='osascript -e \"tell app \"System Events\" to tell appearance preferences to set dark mode to false\"' terminal=false refresh=true"
+# echo "-- Enable Dark Mode  | bash='osascript -e \"tell app \"System Events\" to tell appearance preferences to set dark mode to true\"' terminal=false refresh=true"
+from datetime import datetime, timedelta, date
+import sys
 from dotenv import load_dotenv
 import requests
 import os
-from datetime import datetime, timedelta, date
-now = datetime.now().time().replace(microsecond=0)
-day_in_the_week=datetime.now().strftime("%A")
+import json
+args = sys.argv
+fileName = "tmp/data.json"
 
+now = datetime.now().time().replace(microsecond=0)
+day_in_the_week = datetime.now().strftime("%A")
+todays_date = datetime.now().date()
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
@@ -13,7 +25,29 @@ API_KEY = os.getenv("API_KEY")
 server_api = f"https://islamicapi.com/api/v1/prayer-time/?lat=30.1038551&lon=31.637056&api_key={API_KEY}&method=5"
 
 
+def get_cached_data():
+
+    if not os.path.exists(fileName):
+        return None
+    with open(fileName, "r") as file:
+        data = json.load(file)
+
+        return data
+
+
 def get_prayer_times():
+    cached_data = get_cached_data()
+    if cached_data:
+        date = cached_data["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d").date()
+
+        if date_obj == todays_date:
+
+            kept_values = cached_data["values"]
+            values = list(kept_values.values())
+            print(values, kept_values)
+            time_comparison(now, values, kept_values)
+            return
     fetch = requests.get(
         server_api)
 
@@ -25,8 +59,15 @@ def get_prayer_times():
     values = list(kept_values.values())
 
     values.sort()
-
+    write_new_file_data(kept_values, todays_date)
     time_comparison(now, values, kept_values)
+
+
+def write_new_file_data(values, date):
+    qouta = {"values": values, "date": str(date)}
+    with open(fileName, "w") as file:
+
+        json.dump(qouta, file)
 
 
 def get_tomorrow_fajr(date):
@@ -52,7 +93,7 @@ def time_comparison(client_time, api_times, prayers):
         if client_time < date_format_time:
             next_time = date_format_time.strftime("%H:%M")
             prayer_name = next(k for k, v in prayers.items() if v == time)
-            if day_in_the_week.lower()=="friday" and prayer_name.lower()=="dhuhr":
+            if day_in_the_week.lower() == "friday" and prayer_name.lower() == "dhuhr":
                 print(f"Friday - {next_time}")
                 break
             print(f"{prayer_name} - {next_time}")
@@ -61,10 +102,11 @@ def time_comparison(client_time, api_times, prayers):
     if client_time > isha:
         tomorrow = date.today() + timedelta(days=1)
         prayer_time = get_tomorrow_fajr(tomorrow)
-        print(f"Fajr — {prayer_time}")
+        print(f"Fajr — {prayer_time}") 
+        print(
+            f"---")
+        print("pm-am")
+        print("24-hhours")
 
 
 get_prayer_times()
-# csv
-# txt
-# py
